84e31434f2c897f372459105e36bd5dc367a9940
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-03-22 17:31:57 +0100

    rename roles and add text reports

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,13 +14,15 @@ now=`date +'%Y-%m-%d'`
 report_file_prefix=${ACCOUNT}-${now}
 echo "Generating CloudSploit CIS LEVEL1 report ..."
 ./prowler -g cislevel1 -M csv >${report_file_prefix}-cislevel1.csv
+./prowler -g cislevel1 -M mono >${report_file_prefix}-cislevel1.txt
 
 echo "Generating CloudSploit CIS LEVEL2 report ..."
 ./prowler -g cislevel2 -M csv >${report_file_prefix}-cislevel2.csv
+./prowler -g cislevel2 -M mono >${report_file_prefix}-cislevel2.txt
 
 echo "Saving the report files in s3://${PROWLER_BUCKET}/reports/${ACCOUNT}"
 unset AWS_SECRET_ACCESS_KEY
 unset AWS_ACCESS_KEY_ID
 unset AWS_SESSION_TOKEN
-aws s3 cp . s3://${PROWLER_BUCKET}/reports/${ACCOUNT}/ --exclude="*" --include="${report_file_prefix}*" --recursive
+aws s3 cp . s3://${PROWLER_BUCKET}/reports/${ACCOUNT}/ --exclude="*" --include="${report_file_prefix}-cislevel*" --recursive
 
