205799624c25b3b83ebf01c2c71376a268a7064a
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-04-28 16:23:38 +0200

    Add externalId support to spoke role

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,14 +4,17 @@ TMP_ASSUME_ROLE_FILE=/tmp/assume-role.json
 
 echo "Collecting credentials for ${ACCOUNT} for role ${PROWLER_SCAN_ROLE}"
 aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT}:role/${PROWLER_SCAN_ROLE} \
-	            --role-session-name ${PROWLER_SCAN_ROLE} >${TMP_ASSUME_ROLE_FILE}
+										--external-id ${PROWLER_ROLE_EXTERNALID} \
+	                  --role-session-name ${PROWLER_SCAN_ROLE} \
+										>${TMP_ASSUME_ROLE_FILE}
 
 export AWS_SECRET_ACCESS_KEY=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SecretAccessKey`
-export AWS_ACCESS_KEY_ID=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.AccessKeyId`
-export AWS_SESSION_TOKEN=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SessionToken`
-
 if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then echo "AWS_SECRET_ACCESS_KEY not set !"; exit 1; fi
+
+export AWS_ACCESS_KEY_ID=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.AccessKeyId`
 if [ -z "${AWS_ACCESS_KEY_ID}" ]; then echo "AWS_ACCESS_KEY_ID not set !"; exit 1; fi
+
+export AWS_SESSION_TOKEN=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SessionToken`
 if [ -z "${AWS_SESSION_TOKEN}" ]; then echo "AWS_SESSION_TOKEN not set !"; exit 1; fi
 
 now=`date +'%Y-%m-%d'`
@@ -29,5 +32,7 @@ echo "Saving the report files in s3://${PROWLER_BUCKET}/reports/${ACCOUNT}"
 unset AWS_SECRET_ACCESS_KEY
 unset AWS_ACCESS_KEY_ID
 unset AWS_SESSION_TOKEN
-aws s3 cp . s3://${PROWLER_BUCKET}/reports/${ACCOUNT}/ --exclude="*" --include="${report_file_prefix}-cislevel*" --recursive
-
+aws s3 cp . s3://${PROWLER_BUCKET}/reports/${ACCOUNT}/ \
+						--exclude="*" \
+						--include="${report_file_prefix}-cislevel*" \
+						--recursive
