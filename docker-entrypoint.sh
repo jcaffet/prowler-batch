25619197e095761a6def55132ea9c43db46e245f
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-05-02 15:50:23 +0200

    Merge CIS checks and add Extras checks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "${REPORTING_BUCKET}" ]; then echo "REPORTING_BUCKET not set !"; exit 1;
 if [ -z "${PROWLER_SCAN_ROLE}" ]; then echo "PROWLER_SCAN_ROLE not set !"; exit 1; fi
 if [ -z "${PROWLER_ROLE_EXTERNALID}" ]; then echo "PROWLER_ROLE_EXTERNALID not set !"; exit 1; fi
 
-echo "Collecting credentials for ${ACCOUNT} for role ${PROWLER_SCAN_ROLE}"
+echo "Collecting credentials for ${ACCOUNT} with role ${PROWLER_SCAN_ROLE}"
 aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT}:role/${PROWLER_SCAN_ROLE} \
 										--external-id ${PROWLER_ROLE_EXTERNALID} \
 	                  --role-session-name ${PROWLER_SCAN_ROLE} \
@@ -21,16 +21,16 @@ if [ -z "${AWS_ACCESS_KEY_ID}" ]; then echo "AWS_ACCESS_KEY_ID not set !"; exit 
 export AWS_SESSION_TOKEN=`cat ${TMP_ASSUME_ROLE_FILE} | jq -r .Credentials.SessionToken`
 if [ -z "${AWS_SESSION_TOKEN}" ]; then echo "AWS_SESSION_TOKEN not set !"; exit 1; fi
 
-now=`date +'%Y-%m-%d'`
-report_file_prefix=${ACCOUNT}-${now}
+today=`date +'%Y-%m-%d'`
+report_file_prefix=${ACCOUNT}-${today}-prowler
 
-echo "Generating CIS LEVEL1 report in ${report_file_prefix}-cislevel1.txt ..."
+echo "Generating Prowler CIS checks report in ${report_file_prefix}-cis.txt ..."
 # possible export formats : mono, csv, json, html, ...
-./prowler -g cislevel1 -M mono >${report_file_prefix}-cislevel1.txt
+./prowler -g cislevel2 -M mono >${report_file_prefix}-cis.txt
 
-echo "Generating CIS LEVEL2 report in ${report_file_prefix}-cislevel2.txt ..."
+echo "Generating Prowler Extras checks report in ${report_file_prefix}-extras.txt ..."
 # possible export formats : mono, csv, json, html, ...
-./prowler -g cislevel2 -M mono >${report_file_prefix}-cislevel2.txt
+./prowler -g extras -M mono >${report_file_prefix}-extras.txt
 
 echo "Saving the report files in s3://${REPORTING_BUCKET}/${ACCOUNT}"
 unset AWS_SECRET_ACCESS_KEY
@@ -38,5 +38,5 @@ unset AWS_ACCESS_KEY_ID
 unset AWS_SESSION_TOKEN
 aws s3 cp . s3://${REPORTING_BUCKET}/${ACCOUNT}/ \
 						--exclude="*" \
-						--include="${report_file_prefix}-cislevel*" \
+						--include="${report_file_prefix}-*" \
 						--recursive
